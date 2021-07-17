f17db99281c614e68929b18fab9b6963110e9fc2
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-17 14:49:45 +0300

    Добавление перемещения влево

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,9 +38,13 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.type == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
