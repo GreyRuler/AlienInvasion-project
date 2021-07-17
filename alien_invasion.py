a14c051d1d3e1686fb614f9ff294c054da53a743
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-17 13:17:16 +0300

    Добавление метода _update_screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,18 +27,20 @@ class AlienInvasion:
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
-            # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши."""""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
