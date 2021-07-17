d48de0df1ae1338b730bc7ae4f6c3e756dac262d
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-17 16:32:24 +0300

    Добавление кнопки q для выхода

--- alien_invasion.py
+++ alien_invasion.py
@@ -46,6 +46,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """Реагирует на отпускание клавиш."""
