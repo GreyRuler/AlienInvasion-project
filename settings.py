faf2845f74e942dc16ab9bbd219a228f120e4e5c
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-17 11:43:38 +0300

    Создание класса Setting

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Setting:
+    """Класс для хранения всех настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
