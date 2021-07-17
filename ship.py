43ec7d32986376d6556f721cd40c00a40d5ffa68
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-17 14:45:57 +0300

    Добавление перемещения влево

--- ship.py
+++ ship.py
@@ -15,14 +15,17 @@ class Ship():
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Флаг перемещения
+        # Флаги перемещения
         self.moving_right = False
+        self.moving_left = False
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        """Обновляет позицию корабля с учетом флага."""
+        """Обновляет позицию корабля с учетом флагов."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
