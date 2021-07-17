e94e6f4dc29520306f11ca3ae78d34b50e2ba004
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-17 14:55:37 +0300

    Добавление настройки скорости корабля

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Setting():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (30, 165, 160)
+
+        # Настройка корабля
+        self.ship_speed = 1.5
