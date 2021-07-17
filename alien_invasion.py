602e41e8d8ffa541a62b052cbdbedcb646ab0413
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-17 16:35:20 +0300

    Добавление fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,9 +14,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Setting()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
