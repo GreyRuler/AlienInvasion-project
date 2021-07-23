bb51bbda00a8973d660222cbba2eab9872234a30
DaniilVasilenko <daniil_vasilenko@mail.ru>
2021-07-23 15:23:54 +0300

    Рефакторинг

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,10 +92,15 @@ class AlienInvasion:
         # Создание первого ряда пришельцев.
         for alien_number in range(number_aliens_x):
             # Создание пришельца и размещение его в ряду.
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        """Создание пришельца и размещение его в ряду."""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
